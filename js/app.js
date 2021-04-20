2ce9122d6b6dcb8e776ee4465f89ab92e06b3ae7
danielfernandoyepezvelez <danielfernandoyepezvelez>
2021-04-20 17:49:57 -0500

    Agregando La Creacion Y La Lista De Los Clientes

--- js/app.js
+++ js/app.js
@@ -7,6 +7,10 @@ Salir De Este Archivo */
 
     document.addEventListener('DOMContentLoaded', () => {
         crearDB();
+
+        if (window.indexedDB.open('CRM', 1)) {
+            obtenerClientes();
+        }
     });
 
     /* Crear La Base De Datos De IndexDB */
@@ -35,6 +39,53 @@ Salir De Este Archivo */
             objectStore.createIndex('empresa', 'empresa', { unique: false });
 
             console.log('DB Lista Y Creada');
+        }
+    }
+
+    function obtenerClientes() {
+        const abrirConexion = window.indexedDB.open('CRM', 1);
+
+        abrirConexion.onerror = function() {
+            console.log('Tenemos Un Error En La Conexión Con La Base De Datos');
+        }
+
+        abrirConexion.onsuccess = function() {
+            DB = abrirConexion.result;
+
+            /* Aqui Ya Tenemos El Object Store */
+            const objectStore = DB.transaction('CRM').objectStore('CRM');
+
+            objectStore.openCursor().onsuccess = function(e) {
+                /* Cursor Va Almacenar Los Objetos De La DB */
+                const cursor = e.target.result;
+
+                if (cursor) {
+                    // console.log(cursor.value);
+                    const { nombre, empresa, email, telefono, id } = cursor.value;
+
+                    const listadoClientes = document.querySelector('#listado-clientes');
+                    listadoClientes.innerHTML += ` <tr>
+                        <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                            <p class="text-sm leading-5 font-medium text-gray-700 text-lg  font-bold"> ${nombre} </p>
+                            <p class="text-sm leading-10 text-gray-700"> ${email} </p>
+                        </td>
+                        <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 ">
+                            <p class="text-gray-700">${telefono}</p>
+                        </td>
+                        <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200  leading-5 text-gray-700">    
+                            <p class="text-gray-600">${empresa}</p>
+                        </td>
+                        <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5">
+                            <a href="editar-cliente.html?id=${id}" class="text-teal-600 hover:text-teal-900 mr-5">Editar</a>
+                            <a href="#" data-cliente="${id}" class="text-red-600 hover:text-red-900">Eliminar</a>
+                        </td>
+                    </tr>`;
+
+                    cursor.continue();
+                } else {
+                    console.log('No Tenemos Mas Registros En La Base De Datos.....');
+                }
+            }
         }
     }
 })();
---
+++ js/nuevocliente.js
@@ -0,0 +1,95 @@
+/* Nos Permite Que Las Variables Y La Funciones Que Declaramos
+En Este Archivo SE Queden De Forma Local, Es Decir, No Pueden
+Salir De Este Archivo */
+
+(function() {
+    let DB;
+    const formulario = document.querySelector('#formulario');
+
+    document.addEventListener('DOMContentLoaded', () => {
+        conectarDB();
+
+        formulario.addEventListener('submit', validarCliente);
+    });
+
+    function conectarDB() {
+        const abrirConexion = window.indexedDB.open('CRM', 1);
+
+        abrirConexion.onerror = function() {
+            console.log('Hubo Un Error');
+        }
+
+        abrirConexion.onsuccess = function() {
+            DB = abrirConexion.result;
+        }
+    }
+
+    function validarCliente(e) {
+        e.preventDefault();
+
+        /* Leer Todos Los Inputs Del Formulario */
+        const nombre = document.querySelector('#nombre').value;
+        const email = document.querySelector('#email').value;
+        const telefono = document.querySelector('#telefono').value;
+        const empresa = document.querySelector('#empresa').value;
+
+        if (nombre === '' || email === '' || telefono === '' || empresa === '') {
+            imprimirAlerta('Todos Los Datos Son Obligatorios', 'error');
+            return;
+        }
+
+        /* Objeto Con La Información */
+        const cliente = {
+            id: Date.now(),
+            nombre,
+            email,
+            telefono,
+            empresa,
+        }
+
+        crearNuevoCliente(cliente);
+    }
+
+    function crearNuevoCliente(cliente) {
+        const transaction = DB.transaction(['CRM'], 'readwrite');
+        const objectStore = transaction.objectStore('CRM');
+
+        objectStore.add(cliente);
+
+        transaction.onerror = function() {
+            // console.log('Hubo Un Error Al Ingresar El Registro');
+            imprimirAlerta('Hubo Un Error Al Ingresar El Registro', 'error');
+        }
+
+        transaction.oncomplete = function() {
+            // console.log('Cliente Agregado Correctamente');
+            imprimirAlerta('Cliente Agregado Correctamente', 'success');
+
+            setTimeout(() => {
+                window.location.href = 'index.html';
+            }, 3000);
+        }
+    }
+
+    function imprimirAlerta(mensaje, tipo) {
+        const alerta = document.querySelector('.alerta');
+
+        if (!alerta) {
+            const divMensaje = document.createElement('div');
+            divMensaje.classList.add('px-4', 'py-3', 'rounded', 'max-w-lg', 'mx-auto', 'mt-6', 'text-center', 'border', 'alerta');
+
+            if (tipo === 'error') {
+                divMensaje.classList.add('bg-red-100', 'border-red-400', 'text-red-700');
+            } else {
+                divMensaje.classList.add('bg-green-100', 'border-green-400', 'text-green-700');
+            }
+
+            divMensaje.textContent = mensaje;
+            formulario.appendChild(divMensaje);
+
+            setTimeout(() => {
+                divMensaje.remove();
+            }, 3000);
+        }
+    }
+})();
